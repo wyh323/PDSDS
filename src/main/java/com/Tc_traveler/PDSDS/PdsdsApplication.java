1ba6228dc9bc096f28fd0ca25b55421aca120d5d
Tc_traveler <3249456659@qq.com>
2024-11-07 20:17:59 +0800

    First commit.

---
+++ src/main/java/com/Tc_traveler/PDSDS/PdsdsApplication.java
@@ -0,0 +1,13 @@
+package com.Tc_traveler.PDSDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tc_traveler/PDSDS/PdsdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tc_traveler.PDSDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
