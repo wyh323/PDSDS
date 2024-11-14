afb9cf66bee97c3dd717b8a0c7b12fce9ac71f25
Tc_traveler <3249456659@qq.com>
2024-11-14 21:27:10 +0800

    Add Jwt for login.

--- src/main/java/com/Tc_traveler/PDSDS/controller/PatientController.java
+++ src/main/java/com/Tc_traveler/PDSDS/controller/PatientController.java
@@ -3,12 +3,16 @@ package com.Tc_traveler.PDSDS.controller;
 import com.Tc_traveler.PDSDS.dto.Result;
 import com.Tc_traveler.PDSDS.entity.Patient;
 import com.Tc_traveler.PDSDS.service.UserService;
+import com.Tc_traveler.PDSDS.utils.JwtUtil;
 import com.Tc_traveler.PDSDS.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/patient")
@@ -34,7 +38,11 @@ public class PatientController {
             return Result.error("用户名不存在");
         }
         if(Md5Util.getMD5String(password).equals(patient.getPassword())){
-            return Result.success("return JWT.");
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",patient.getId());
+            claims.put("username",patient.getUsername());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
         }else {
             return Result.error("密码错误!");
         }
